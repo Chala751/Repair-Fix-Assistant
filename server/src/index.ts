f9e1300a2e67e557dab211ab7bb8accabb080e92
Chala Temesgen <ctemesgen85@gmail.com>
2025-12-15 15:46:09 +0300

    server running

---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+//import chatRouter from "./routes/chat";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//app.use("/chat", chatRouter);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}`);
+});
