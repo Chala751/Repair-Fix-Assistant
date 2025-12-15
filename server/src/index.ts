ab20fbcc45a891cfc85cb9a6f463efedaed91c1e
Chala Temesgen <ctemesgen85@gmail.com>
2025-12-15 15:58:03 +0300

    the repair fix id done with dummy data not let use the real one

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-//import chatRouter from "./routes/chat";
+import chatRouter from "./routes/chat";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-//app.use("/chat", chatRouter);
+app.use("/chat", chatRouter);
 
 const PORT = 5000;
 app.listen(PORT, () => {
