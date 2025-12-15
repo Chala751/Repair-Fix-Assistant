6241ac12f2c6970206fe4382056e9ac223520a8e
Chala Temesgen <ctemesgen85@gmail.com>
2025-12-15 15:50:31 +0300

    cleanup done

---
+++ server/src/utils/cleanup.ts
@@ -0,0 +1,7 @@
+export function cleanIFixitJSON(raw: any) {
+  return raw.map((item: any) => ({
+    title: item.display_title,
+    text: item.text,
+    images: item.image ? Object.values(item.image) : [],
+  }));
+}
